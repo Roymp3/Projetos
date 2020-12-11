aae3829bd0fa6494f13abb16d26e8bbd1eed9da8
Roymp3 <71888012+Roymp3@users.noreply.github.com>
2020-12-10 21:02:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
 function operacao(event){
        
-            let n1 = parseFloat(document.getElementById("num1").value)
-            let n2 = parseFloat(document.getElementById("num2").value)
+            let n1 = String(document.getElementById("cpf").value)
+            var n2
             let op = event.target.textContent
             let resultado
 
-            switch(op){
+          if(op == "Verificar cpf"){
 
-                case "+":
-                     console.log("soma");
-                     resultado = n1 + n2;
-                     break;
-                case "-":
-                     console.log("subtração");
-                     resultado = n1 - n2;
-                     break;
-                case "*":
-                     console.log("multiplicação");
-                     resultado = n1 * n2;
-                     break;
-                case "/":
-                     console.log("divisão");
-                     resultado = n1 / n2;
-                     break;
+               console.log("validar");
+               n2 = n1.substring(9,8)
+               if (n2 == 8) {
+                    resultado = "SP"
+               }
+               else if(n2 == 0){
 
+                    resultado = "RS"
+               }
+               else if(n2 == 1){
 
-            }
+                    resultado = "Df, GO, MS e TO"
+               }
+               else if(n2 == 2){
+
+                    resultado = "AM, PA, RR, AP, AC e RO"
+               }
+               else if(n2 == 3){
+
+                    resultado = "CE, MA e PI"
+               }
+               else if(n2 == 4){
+
+                    resultado = "PB, PE, AL e RN"
+               }
+               else if(n2 == 5){
+
+                    resultado = "BA e SE"
+               }
+               else if(n2 == 6){
+
+                    resultado = "MG"
+               }
+               else if(n2 == 7){
+
+                    resultado = "RJ e ES"
+               }
+               else if(n2 == 9){
+
+                    resultado = "PR e SC"
+               }
+
+          }
             document.getElementById("resultado").textContent = resultado
 }           
